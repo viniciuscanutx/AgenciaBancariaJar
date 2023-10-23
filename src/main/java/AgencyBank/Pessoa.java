3bb8d728ca8b0e0e9c4f31deec83e3abe0161339
viniciuscanutx <viniciuscanutmz@gmail.com>
2023-10-23 12:36:12 -0300

    Setando a classe pessoa e preparando os arquivos para a formatação do valor.

--- src/main/java/AgencyBank/Pessoa.java
+++ src/main/java/AgencyBank/Pessoa.java
@@ -8,4 +8,44 @@ public class Pessoa {
     private String cpf;
     private String email;
 
+    public Pessoa(String nome, String cpf, String email) {
+
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        contador1 += 1;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "\nNome: " + this.getNome() +
+                "\nCPF: " + this.getCpf() +
+                "\nE-mail: " + this.getEmail();
+
+    }
+
 }
