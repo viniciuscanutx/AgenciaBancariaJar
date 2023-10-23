e08eeb432b22fe5abf062b9e342ab434b24c4cf3
viniciuscanutx <viniciuscanutmz@gmail.com>
2023-10-23 16:42:36 -0300

    Programa finalizado com as funções sacar, transferir, depositar e listar contas.

--- src/main/java/AgencyBank/App01.java
+++ src/main/java/AgencyBank/App01.java
@@ -58,22 +58,101 @@ public class App01 {
 
     public static void criarConta() {
 
+        System.out.println("\nNome: ");
+        String nome = input.next();
+
+        System.out.println("\nCPF: ");
+        String cpf = input.next();
+
+        System.out.println("\nE-mail: ");
+        String email = input.next();
+
+        Pessoa pessoa = new Pessoa(nome, cpf, email);
+        Conta conta = new Conta(pessoa);
+
+        contasBancarias.add(conta);
+        System.out.println("Sua conta foi criada!");
+
+        operacoes();
+
+    }
+
+    private static Conta encontrarConta(int numeroConta) {
+
+        Conta conta = null;
+        if (contasBancarias.size() > 0) {
+            for (Conta c : contasBancarias) {
+                if(c.getAccountnumber() == numeroConta) {
+                    conta = c;
+                }
+            }
+        }
+        return conta;
     }
 
     public static void depositar() {
 
+        System.out.println("Número da Conta: ");
+        int numConta = input.nextInt();
+        Conta conta = encontrarConta(numConta);
+
+        if(conta != null){
+            System.out.println("Valor para Deposito: ");
+            double valorDepositado = input.nextDouble();
+            conta.depositar(valorDepositado);
+            System.out.println("Valor depositado com sucesso!");
+        } else {
+            System.out.println("A conta não existe");
+        }
+        operacoes();
     }
 
     public static void sacar() {
 
+        System.out.println("Número da Conta: ");
+        int numConta = input.nextInt();
+        Conta conta = encontrarConta(numConta);
+
+        if(conta != null){
+            System.out.println("Valor para sacar: ");
+            double valorsaque = input.nextDouble();
+            conta.sacar(valorsaque);
+        } else {
+            System.out.println("A conta não existe");
+        }
+        operacoes();
     }
 
     public static void transferir() {
 
+        System.out.println("Número da Conta de Origem: ");
+        int numOrigem = input.nextInt();
+        Conta origem = encontrarConta(numOrigem);
+
+        if(origem != null) {
+            System.out.println("Número da Conta Destino: ");
+            int destino = input.nextInt();
+            Conta destino2 = encontrarConta(destino);
+            if(destino2 != null) {
+                System.out.println("Quantia a ser Transferida: ");
+                double quantidadeTransferida = input.nextDouble();
+                origem.transferir(destino2, quantidadeTransferida);
+            }  
+        } 
+        operacoes();     
     }
 
     public static void listarcontas() {
+        if(contasBancarias.size() > 0 ){
+            for(Conta conta: contasBancarias) {
+                System.out.println(conta);
+            }
+        } else {
+                System.out.println("Não há contas cadastradas!");
+            }
+            operacoes();
+        }
 
-    }
 
 }
+
